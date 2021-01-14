b5c9f2f2d6652f721affe187781658bd707b44a7
PadoSensei <padraigo.donoghue@gmail.com>
2021-01-14 00:50:51 +0000

    Added Review and Stripe Frontend

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -9,9 +9,11 @@ import PaymentForm from '../PaymentForm/PaymentForm';
 const steps = ['Shipping address', 'Payment details']
 
 const Checkout = ({ cart }) => {
-  const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [shippingData, setShippingData] = useState({});
   const classes = useStyles();
+  
 
   useEffect(() => {
     const generateToken = async () => {
@@ -27,6 +29,14 @@ const Checkout = ({ cart }) => {
     generateToken()
   }, [cart]);
 
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next =(data) => {
+    setShippingData(data);
+    nextStep();
+  }
+
   const Confirmation = () => {
     return (
       <div>
@@ -36,8 +46,8 @@ const Checkout = ({ cart }) => {
   }
 
   const Form = () => activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} />
-    : <PaymentForm />
+    ? <AddressForm checkoutToken={checkoutToken} next={next} />
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
 
      
    return (
